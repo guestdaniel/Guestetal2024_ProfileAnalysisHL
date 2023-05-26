976b6d752e7d322a581b407b284562e6e5c8223f
Daniel Guest <guest121@umn.edu>
2023-05-26 11:10:29 -0400

    Increased sampling density for PF estimates
    Increased sampling density for PF estimates and also added a
    -999.9 dB SRS condition to provide an easy fix for testing -Inf vs
    -Inf to estimate noise floor.

--- src/experiments/genfigs.jl
+++ src/experiments/genfigs.jl
@@ -2,7 +2,58 @@ export genfig_zeta_c1_psychometric_functions,
        genfig_zeta_c2_psychometric_functions,
        genfig_zeta_c3_psychometric_functions
 
-function genfig_zeta_c1_psychometric_functions(increments=vcat(-Inf, -30.0:5.0:5.0))
+function genfig_eta_b1_rate_curves(increments=[-20.0, -15.0, -10.0, -5.0], config::Config=Default())
+    # Set up experiment
+    exp = ProfileAnalysis_AvgPatterns()
+
+    # Set up plot
+    set_theme!(theme_carney; fontsize=10.0)
+    fig = Figure(; resolution=(360, 125))
+    axs = [Axis(fig[1, i]) for i in 1:4]
+    [xlims!(ax, -30.0, 3.0) for ax in axs]
+    [ax.xticks = (-25.0:5.0:0.0, vcat("-Inf", string.(Int.(-20.0:5.0:0.0)))) for ax in axs]
+
+    # Fetch models 
+    models = setup(exp, 1000.0)
+
+    # Do each plot, on-CF coding
+    for (ax, model) in zip(axs, models[1:1])
+        # Calculate CFs and middle index
+        cf = log2.(model.cf ./ 1000.0)
+        idx = Int(round(length(cf)/2))
+
+        # Construct simulations and run
+        out = map(increments) do increment
+            ref = setup(exp, model, -Inf, 1000.0, 21)
+            tar = setup(exp, model, increment, 1000.0, 21)
+            sim = DeltaPattern(; pattern_1=ref, pattern_2=tar)
+            out = @memo config simulate(sim)
+            [x[idx] for x in out]
+        end
+
+        # Preprocess to transform into z-scored rates
+        μ = map(x -> mean(x)/std(x), out)
+
+        # Plot results
+        hlines!(ax, [0.0]; color=:black, linewidth=1.0)
+        scatter!(ax, increments, μ; color=:black)
+        lines!(ax, increments, μ; color=:black)
+    end
+
+    # Adjust
+    [ylims!(ax, -5.0, 5.0) for ax in axs]
+    [ax.xticklabelrotation = π/2 for ax in axs]
+    axs[1].ylabel = "Normalized Δ rate"
+    Label(fig[2, 2:3], "Increment (dB SRS)"; tellwidth=false)
+    rowgap!(fig.layout, 1, Relative(0.03))
+    colgap!(fig.layout, Relative(0.02))
+    neaten_grid!(axs)
+
+    # Return
+    return fig
+end
+
+function genfig_zeta_c1_psychometric_functions(increments=vcat(-999.9, -45.0:2.5:5.0))
     # Set experiment
     exp = ProfileAnalysis_PFObserver()
 
@@ -42,7 +93,7 @@ function genfig_zeta_c1_psychometric_functions(increments=vcat(-Inf, -30.0:5.0:5
     return fig
 end
 
-function genfig_zeta_c2_psychometric_functions(increments=vcat(-Inf, -30.0:5.0:5.0))
+function genfig_zeta_c2_psychometric_functions(increments=vcat(-999.9, -45.0:2.5:5.0))
     # Set experiment
     exp = ProfileAnalysis_PFObserver()
 
@@ -90,7 +141,7 @@ function genfig_zeta_c2_psychometric_functions(increments=vcat(-Inf, -30.0:5.0:5
     return fig
 end
 
-function genfig_zeta_c3_psychometric_functions(increments=vcat(-Inf, -30.0:5.0:5.0))
+function genfig_zeta_c3_psychometric_functions(increments=vcat(-999.9, -45.0:2.5:5.0))
     # Set experiment
     exp = ProfileAnalysis_PFTemplateObserver()
 
