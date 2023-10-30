2b1f8085ca7e2a21b7944030792cddf08064ce6f
Daniel Guest <guest121@umn.edu>
2023-10-30 11:21:34 -0400

    Adjusted amount of RAM for BlueHive
    Adjusted amount of RAM allocated to each process for running on BlueHive, since we unexpectedly got a memory-exceeded error while running sims over the weekend

--- src/experiments/PFs/PFs_run.sh
+++ src/experiments/PFs/PFs_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=standard
-#SBATCH --cpus-per-task=32
-#SBATCH --mem-per-cpu=2G
+#SBATCH --cpus-per-task=30
+#SBATCH --mem-per-cpu=2.5G
 #SBATCH --time=96:00:00 
 #SBATCH --output=/home/dguest2/ProfileAnalysis.log
 #SBATCH --mail-type=ALL
@@ -18,7 +18,7 @@ echo "Start time : $now"
 
 # Run script
 cd ~/cl_code/ProfileAnalysis/src/experiments/PFs
-julia -p 30 PFs_run.jl
+julia -p 28 PFs_run.jl
 
 # Print current time
 now=$(date +"%T")
